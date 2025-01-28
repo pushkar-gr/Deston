b82382ae69cd76359878363be9ca7e21da883cf1
pushkar-gr <gr.pushkar.7@gmail.com>
2025-01-28 19:46:09 +0530

    documentation

--- src/load_balancer/load_balancer.rs
+++ src/load_balancer/load_balancer.rs
@@ -1,5 +1,8 @@
 pub trait LoadBalancer {
+    //returns a LoadBalancer
     fn new() -> Self;
+    //starts the load balancer
     async fn start(&self) -> Result<(), Box<dyn std::error::Error + Send + Sync>>;
+    //stops the load balancer
     fn stop(&self);
 }
