dd4a462b4e44766afbac65bb4876f1b6bf7548cb
pushkar-gr <gr.pushkar.7@gmail.com>
2025-02-07 22:17:31 +0530

    add: config parser

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,12 +1,16 @@
 //defines Config struct that holds list of structures, selected load balancer algorithm, and an algorithm object.
 
+use hyper::Uri;
+use std::fs;
+use std::path::Path;
 use std::sync::{Arc, Mutex};
+use toml::{Table, Value};
 
 use crate::load_balancer::algorithm::algorithm::Algorithm as AlgorithmTrait;
 use crate::load_balancer::algorithm::r#static::{
     ip_hashing::IpHashing, round_robin::RoundRobin, weighted_round_robin::WeightedRoundRobin,
 };
-use crate::server::server::SyncServer;
+use crate::server::server::{Server, SyncServer};
 
 //type alias for a thread-safe, synchronized Config using Arc and Mutex
 pub type SyncConfig = Arc<Mutex<Config>>;
@@ -20,6 +24,7 @@ pub enum Algorithm {
 }
 
 pub struct Config {
+    pub load_balancer_address: Uri,    //address of load balancer
     pub servers: Arc<Vec<SyncServer>>, //thread safe vector of servers
     pub algorithm: Algorithm,          //algorithm to pick server
     pub last_picked_index: usize,      //index of last picked server
@@ -28,19 +33,123 @@ pub struct Config {
 
 impl Config {
     //creates and returns a new Config
-    pub fn new(servers: Arc<Vec<SyncServer>>, algorithm: Algorithm) -> Self {
+    pub fn new(config_path: &Path) -> Self {
+        //read contents of config file
+        let contents = fs::read_to_string(config_path).unwrap();
+        //parese config file contents
+        let values = contents.parse::<Table>().unwrap();
+
+        //get host name of load balancer
+        let load_balancer_host = {
+            if let Value::String(address) =
+                values.get("load_balancer").unwrap().get("address").unwrap()
+            {
+                address
+            } else {
+                //if host not found in config
+                &"localhost".to_string()
+            }
+        };
+        //get port of load balancer
+        let load_balancer_port = {
+            if let Value::Integer(port) = values.get("load_balancer").unwrap().get("port").unwrap()
+            {
+                port
+            } else {
+                //if port not found in config
+                &8080
+            }
+        };
+        //get algorithm for load balancer
+        let algorithm: Algorithm = {
+            if let Value::String(algo) = values
+                .get("load_balancer")
+                .unwrap()
+                .get("algorithm")
+                .unwrap()
+            {
+                get_algorithm(algo)
+            } else {
+                //if algorithm not found in config
+                Algorithm::RoundRobin
+            }
+        };
+
+        //create Config
         Self {
-            last_picked_index: servers.len(),
-            servers: servers,
-            algorithm: algorithm.clone(),
+            //address of load balancer
+            load_balancer_address: (load_balancer_host.to_owned()
+                + ":"
+                + &load_balancer_port.to_string())
+                .parse::<Uri>()
+                .unwrap(),
+
+            //list of servers
+            servers: {
+                //get servers table
+                if let Value::Array(servers) = values.get("server").unwrap() {
+                    Arc::new(
+                        servers
+                            .iter()
+                            .map(|server| {
+                                //get server host
+                                let server_host = {
+                                    if let Value::String(address) = server.get("address").unwrap() {
+                                        address
+                                    } else {
+                                        &"localhost".to_string()
+                                    }
+                                };
+                                //get server port
+                                let server_port = {
+                                    if let Value::Integer(port) = server.get("port").unwrap() {
+                                        port
+                                    } else {
+                                        &3000
+                                    }
+                                };
+                                //create new server object
+                                Arc::new(Mutex::new(Server::new(
+                                    (server_host.to_owned() + ":" + &server_port.to_string())
+                                        .parse::<Uri>()
+                                        .unwrap(),
+                                )))
+                            })
+                            .collect(),
+                    )
+                } else {
+                    //if servers not found in config
+                    let server1 = Server::new("http://127.0.0.1:3000".parse::<Uri>().unwrap());
+                    let server2 = Server::new("http://127.0.0.1:3001".parse::<Uri>().unwrap());
+                    Arc::new(vec![
+                        Arc::new(Mutex::new(server1)),
+                        Arc::new(Mutex::new(server2)),
+                    ])
+                }
+            },
             algorithm_object: {
                 //pick algorithm based on input
-                match algorithm {
+                match &algorithm {
                     Algorithm::RoundRobin => Box::new(RoundRobin::new()),
                     Algorithm::WeightedRoundRobin => Box::new(WeightedRoundRobin::new()),
                     Algorithm::IpHashing => Box::new(IpHashing::new()),
                 }
             },
+            algorithm: algorithm,
+            last_picked_index: 0,
         }
+    }
+}
+
+//funciton to get Algorithm from string
+fn get_algorithm(algorithm: &String) -> Algorithm {
+    if algorithm == "RoundRobin" {
+        Algorithm::RoundRobin
+    } else if algorithm == "WeightedRoundRobin" {
+        Algorithm::WeightedRoundRobin
+    } else if algorithm == "IpHashing" {
+        Algorithm::IpHashing
+    } else {
+        Algorithm::RoundRobin
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,16 @@ mod config;
 mod load_balancer;
 mod server;
 use crate::load_balancer::load_balancer::LoadBalancer;
-use config::config::{Algorithm, Config};
+use config::config::Config;
 use load_balancer::layer4::Layer4;
 use load_balancer::layer7::Layer7;
-use server::server::Server;
+use std::path::Path;
 
-use hyper::Uri;
 use std::sync::{Arc, Mutex};
 
 #[tokio::main]
 async fn main() {
-    let server1 = Server::new("http://127.0.0.1:3000".parse::<Uri>().unwrap());
-    let server2 = Server::new("http://127.0.0.1:3001".parse::<Uri>().unwrap());
-    let config = Config::new(
-        Arc::new(vec![
-            Arc::new(Mutex::new(server1)),
-            Arc::new(Mutex::new(server2)),
-        ]),
-        Algorithm::RoundRobin,
-    );
-    //let lb = Layer7::new(Arc::new(Mutex::new(config)));
+    let config = Config::new(Path::new("config.toml"));
     let lb = Layer4::new(Arc::new(Mutex::new(config)));
     let _ = lb.start().await;
 }
